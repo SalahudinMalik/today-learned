c3eed27260348935ac97f4eb57504c61eb9a7597
Muhammad Salahudin <salahudiniiui@gmail.com>
2018-07-27 14:03:55 +0500

    Update node.js

--- node.js
+++ node.js
@@ -1,2 +1,4 @@
 use this loop :  for (let key in obj) {}
 instead of :  _.forEach(newdata, async function (value, key) {})
+
+// jsreport is just an api you have to design report and send data from node to it.
